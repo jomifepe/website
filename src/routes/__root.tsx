40842c0368e461240065da69a6608696158d298d
José Pereira <7235666+jomifepe@users.noreply.github.com>
2026-01-27 01:27:30 +0000

    feat: title

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,7 +15,7 @@ export const Route = createRootRoute({
 				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: "josé pereira",
+				title: "José Pereira",
 			},
 		],
 		links: [
